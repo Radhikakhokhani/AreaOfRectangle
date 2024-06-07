b437494d61e5cce25b1a7fae39b45411b40bac37
Radhikakhokhani <164177157+Radhikakhokhani@users.noreply.github.com>
2024-06-07 12:00:26 +0530

    DemoPerson.java

--- person/DemoPerson.java
+++ person/DemoPerson.java
@@ -7,11 +7,11 @@ import tnsif.region.countrystatepack.Country;
 public class DemoPerson {
     public static void main(String[] args) {
         // Create City and Country objects
-        City city = new City("JUNAGADH");
+        City city = new City("Surat");
         Country country = new Country("India");
 
         // Create a Person object
-        Person person = new Person("Bansi", city, country);
+        Person person = new Person("Radhika", city, country);
 
         // Print the Person object
         System.out.println(person);
